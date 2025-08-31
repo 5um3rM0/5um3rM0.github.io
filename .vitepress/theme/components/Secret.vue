fed337c14d7b859ae47d4a5f2182ed9b28197fce
5um3rM0 <zjr18879799096@gmail.com>
2025-08-31 18:37:51 +0800

    fix: 页面样式

--- .vitepress/theme/components/Secret.vue
+++ .vitepress/theme/components/Secret.vue
@@ -73,7 +73,6 @@
           <Content id="page-content" class="markdown-main-style" />
           <!-- 秘密文章不显示版权、推荐和下一篇等社交组件 -->
         </article>
-        <!-- 修复：将Aside组件包裹在具有"main-aside"类的div中 -->
         <div class="main-aside">
           <Aside showToc />
         </div>
@@ -124,7 +123,184 @@ onMounted(() => {
 </script>
 
 <style lang="scss" scoped>
-@use "../style/post.scss";
+.post {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  animation: fade-up 0.6s 0.1s backwards;
+
+  .post-meta {
+    padding: 2rem 0 3rem 18px;
+    width: 100%;
+
+    .meta {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      .categories {
+        margin-right: 12px;
+
+        .cat-item {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          padding: 6px 12px;
+          font-size: 14px;
+          font-weight: bold;
+          border-radius: 8px;
+          background-color: var(--main-mask-Inverse-background);
+          opacity: 0.8;
+
+          .iconfont {
+            margin-right: 6px;
+          }
+
+          &:hover {
+            color: var(--main-color);
+            background-color: var(--main-color-bg);
+
+            .iconfont {
+              color: var(--main-color);
+            }
+          }
+        }
+      }
+
+      .tags {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        .tag-item {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          padding: 6px 12px;
+          font-size: 14px;
+          font-weight: bold;
+          border-radius: 8px;
+          opacity: 0.8;
+
+          .iconfont {
+            margin-right: 4px;
+            opacity: 0.6;
+            font-weight: normal;
+          }
+
+          &:hover {
+            color: var(--main-color);
+            background-color: var(--main-color-bg);
+
+            .iconfont {
+              color: var(--main-color);
+            }
+          }
+        }
+      }
+    }
+
+    .title {
+      font-size: 2.2rem;
+      line-height: 1.2;
+      color: var(--main-font-color);
+      margin: 1.4rem 0;
+    }
+
+    .other-meta {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      .meta {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 6px 12px;
+        font-size: 14px;
+        border-radius: 8px;
+        opacity: 0.8;
+
+        .iconfont {
+          margin-right: 6px;
+          transition: color 0.3s;
+        }
+
+        &.date {
+          padding-left: 0;
+        }
+      }
+    }
+  }
+
+  .post-content {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    animation: fade-up 0.6s 0.3s backwards;
+
+    .post-article {
+      width: calc(100% - 300px);
+      padding: 1rem 2.2rem 2.2rem 2.2rem;
+      user-select: text;
+      cursor: auto;
+
+      &:hover {
+        border-color: var(--main-card-border);
+      }
+    }
+
+    .main-aside {
+      width: 300px;
+      padding-left: 1rem;
+    }
+
+    @media (max-width: 1200px) {
+      .post-article {
+        width: 100%;
+      }
+
+      .main-aside {
+        display: none;
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    .post-meta {
+      padding: 4rem 1.5rem;
+
+      .meta {
+        justify-content: center;
+
+        .categories {
+          margin-right: 0;
+        }
+
+        .tags {
+          display: none;
+        }
+      }
+
+      .title {
+        font-size: 1.6rem;
+        text-align: center;
+        line-height: 40px;
+      }
+
+      .other-meta {
+        justify-content: center;
+      }
+    }
+
+    .post-content {
+      .post-article {
+        border: none;
+        padding: 20px 30px;
+      }
+    }
+  }
+}
 
 .secret-layout,
 .post {
